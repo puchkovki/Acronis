1c7c4a790df70c4242f03b08f45d27ea7312a3fd
puchkovki <puchkov.k@phystech.edu>
2020-02-08 21:16:50 +0300

    Added FizzBuzz solution

--- FindNum.cpp
+++ FindNum.cpp
@@ -2,30 +2,38 @@
 #include <vector>
 
 int main(void) {
-    int arraySize, M, number;
-    std::vector<int> array, temp;
+    // "Input" array and it's size
+    std::vector<int> array;
+    int array_size;
+    // Auxiliary array and it's size
+    std::vector<int> domain;
+    int M;
+
     std::cout << "Введите M и размер массива" << std::endl;
-    std::cin >> M >> arraySize;
+    std::cin >> M >> array_size;
+
+    // Auxiliary variable for reading
+    int number;
     std::cout << "Введите массив" << std::endl;
-    for(int i = 0; i < arraySize; i++){
+    // Filling the "input" array
+    for(int i = 0; i < array_size; i++) {
         std::cin >> number;
         array.push_back(number);
     }
-    for(int i = 0; i < M; i++){
-        temp.push_back(i);
+    // Filling the auxiliary array
+    for(int i = 0; i < M; i++) {
+        domain.push_back(i);
     }
-    /*for (auto i: array){
-        std::cout << i;
+
+    // Marking the auxiliary array's matches with the "input" one 
+    for(auto i: array) {
+        domain[i] = -1; 
     }
-    for (auto i: temp){
-        std::cout << i;
-    }*/
-    for(auto i: array){
-        temp[i] = -1; 
-    }
-    for(int i = 0; i < M; i++){
-        if(temp[i] != -1){
-            std::cout << temp[i] << " ";
+
+    // All unmarked elements are the missing ones
+    for(int i = 0; i < M; i++) {
+        if(domain[i] != -1) {
+            std::cout << domain[i] << " ";
         } 
     }
     std::cout << std::endl;
---
+++ FizzBuzz.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+
+// The number is divisible by 3, if the numerals' sum is divisible by 3
+bool divisible_by_three(std::string number) {
+    int numeral = 0;
+    
+    if(number[0] == '-') {
+        number.erase(0, 1);
+    }
+
+    // Iterative summation of numbers' numerals
+    while(number.length() > 1) {
+        numeral = 0;
+        for(auto i: number) {
+            numeral += i - '0';
+        }
+        number = std::to_string(numeral);
+    }
+    
+
+    switch(number[0]) {
+        case '0':
+            return true;
+        case '3':
+            return true;
+        case '6':
+            return true;
+        case '9':
+            return true;
+    }
+
+    return false;
+}
+
+int main(void) {
+    // Reading and checking number for divisibility
+    std::string number;
+    while(std::cin >> number) {
+        std::string fizzbuzz = "fizzbuzz";
+        
+        // The number is divisible by 5, if the last numeral is '0' or '5'
+        if((number[number.length() - 1] != '0') && (number[number.length() - 1] != '5')) {
+            fizzbuzz.erase(4);
+        }
+
+        if(!divisible_by_three(number)) {
+            fizzbuzz.erase(0, 4);
+        }
+
+        // Not changing the number if it's not divisible by '3' and '5'
+        // Printing the answer
+        if(fizzbuzz.length() != 0) {
+            std::cout << fizzbuzz << " ";
+        } else {
+            std::cout << number << " ";
+        }
+    } 
+    
+    std::cout << std::endl;
+}
