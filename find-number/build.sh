951b988e218f1f69e9b317627e4f51b85e79d637
puchkovki <puchkov.k@phystech.edu>
2020-11-27 17:14:30 +0200

    refactor: Organize code structure

--- find-number/build.sh
+++ find-number/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 FULLDIR=$(dirname "$0")
-g++ -Wall -Wextra -pedantic -std=c++17 -O3 -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -fsanitize=address -fsanitize=undefined -fno-sanitize-recover=all -fstack-protector -o $FULLDIR/test $FULLDIR/solution.cpp || exit 1
+g++ -Wall -Wextra -pedantic -std=c++17 -O3 -Wshadow -Wformat=2 -Wfloat-equal \
+-Wconversion -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC \
+-fsanitize=address -fsanitize=undefined -fno-sanitize-recover=all -fstack-protector \
+-o $FULLDIR/test $FULLDIR/solution.cpp || exit 1
 $FULLDIR/test
 exit $?
